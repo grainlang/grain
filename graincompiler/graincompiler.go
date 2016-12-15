f6ecbb570495b4dcf893de454ca5fd0c0eaf83dc
Maciej Górski <maciek.gorski@gmail.com>
2016-12-15 19:31:24 +0100

    Create object file from IR.
    To compile file, run: clang hello. -o hello

--- graincompiler/graincompiler.go
+++ graincompiler/graincompiler.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"llvm.org/llvm/bindings/go/llvm"
+	"fmt"
+	"os"
+	"io/ioutil"
 )
 
 func main() {
@@ -14,14 +17,51 @@ func main() {
 
 	mainFuncType := llvm.FunctionType(llvm.VoidType(), []llvm.Type{}, false)
 	mainFunc := llvm.AddFunction(mainModule, "main", mainFuncType)
-	//
+
 	body := llvm.AddBasicBlock(mainFunc, "entry")
 	builder.SetInsertPoint(body, mainFunc)
 
-	hello := builder.CreateGlobalStringPtr("hello", "hello")
+	hello := builder.CreateGlobalStringPtr("Hello, Grainlang!", "hello")
 	builder.SetInsertPoint(body, body.FirstInstruction())
 	builder.CreateCall(putsFunc, []llvm.Value{hello}, "puts2")
 	builder.CreateRetVoid()
 
 	mainModule.Dump()
+
+	var err error
+	var target llvm.Target
+
+	llvm.LinkInMCJIT()
+
+	err = llvm.InitializeNativeTarget()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Native target initialization error:")
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+
+	err = llvm.InitializeNativeAsmPrinter()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "ASM printer initialization error:")
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+
+	target, err = llvm.GetTargetFromTriple(llvm.DefaultTargetTriple())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Cannot get target:")
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+
+	fmt.Println("Initialize: TargetTriple = " + llvm.DefaultTargetTriple())
+	fmt.Println("Initialize: TargetDescription = " + target.Description())
+
+	machine := target.CreateTargetMachine(llvm.DefaultTargetTriple(),
+		"", "",
+		llvm.CodeGenLevelNone,
+		llvm.RelocDefault,
+		llvm.CodeModelSmall)
+	buffer, err := machine.EmitToMemoryBuffer(mainModule, llvm.ObjectFile)
+	ioutil.WriteFile("hello.o", buffer.Bytes(), 0644)
 }
